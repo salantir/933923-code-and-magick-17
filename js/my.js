9e12142d15f6ea92601cb19f8fa7e384266e26a2
Alex <salantir@mail.ru>
2019-06-30 12:30:17 +0300

    исправления 2

--- js/my.js
+++ js/my.js
@@ -4,7 +4,7 @@ var fireballSize = 22;
 var wizardSpeed = 3;
 var wizardWidth = 70;
 
-var getFireballSpeed = function(left) {
+var getFireballSpeed = function (left) {
   if (left) {
     return 5;
   }
@@ -13,19 +13,19 @@ var getFireballSpeed = function(left) {
 };
 
 // Вычесляем высоту мага
-var getWizardHeight = function() {
+var getWizardHeight = function () {
   var proportionalHeight = 1.337 * wizardWidth;
   return proportionalHeight;
 };
 
 // Положение мага по горизонтали
-var getWizardX = function(width) {
+var getWizardX = function (width) {
   var wizardX = width / 3 + wizardWidth / 2;
-return wizardX;
+  return wizardX;
 };
 
-//Положение мага по вертикали
-var getWizardY = function(height) {
-  var wizardY = height * 2 / 60;
-return wizardY;
+// Положение мага по вертикали
+var getWizardY = function (height) {
+  var wizardY = height * 2 / 30;
+  return wizardY;
 };
